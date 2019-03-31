69275f0c78e865d341331aa2834988148eeaab81
Leo Feldman <laf22@duke.edu>
2019-03-31 14:27:04 -0400

    added channel mention, random header, and more message options

--- main.go
+++ main.go
@@ -17,14 +17,21 @@ func main() {
 		log.Fatal("Need a non-empty slack token - check the SWAT_BOT_SLACK_TOKEN environment variable.")
 	}
 
-	ticker := time.NewTicker(time.Second * 2)
+	ticker := time.NewTicker(time.Minute * 20)
 	defer ticker.Stop()
 	done := make(chan bool)
 	go func() {
-		time.Sleep(10 * time.Second)
+		time.Sleep(24 * time.Hour)
 		done <- true
 	}()
 	postMessageURL := "https://slack.com/api/chat.postMessage"
+	headers := []string{
+		"This just in",
+		"Just in from App Support",
+		"Fresh from the release",
+		"[Escalated]",
+		"Heard from Client Care",
+	}
 	swatAlerts := []string{
 		"Reports are up sitewide - an unexpected success has occurred.",
 		"None of lfeldmandemo's 17,000 users can log in!",
@@ -33,19 +40,23 @@ func main() {
 		"SAML is not working in China!!",
 		"Germany has just reported that Assessments are not working!!",
 		"Referrer URL E2E tests are failing!!",
-		"🦸‍♂️ADMIN user BROKEN!!!",
+		"SUPERHEROADMIN user BROKEN!!!",
 		"Academy users are maybe there, but maybe they aren't!",
 		"Users are unable to fit their certificates into frames when printed!",
 		"Data export utility has been running non-stop for 1 week!",
 		"Enable HTTPS organization setting has been accidentally enabled for every site!",
 		"Hierarchy tree with depth = 1,000,000 causing stack overflow!",
-		"All web services are returning a -900 - What could it mean?",
+		"All web services are returning a -900. What could it mean?",
 		"Request blip as azure web app was swapped - learner logged in one second slower.",
 		"Rapid user save completely deletes the user and all associated CE credits.",
 		"Authentication ticket did not account for Untraditional SSO - \"historic\" customers broken.",
 		"User field was blanked out when blank value was provided!",
 		"UpdateUser13 is returning a -600.",
 		"Competency Tracker Reporter sees the \"Internal Reports\" report.",
+		"LMS Account creation is happening too fast, causing token redemption successes on Academy.",
+		"Site is down due to F5 key on the My Learning page.",
+		"Site2 users can't proxy login into other sites!",
+		"The newsfeed section has magically reappeared!",
 	}
 
 	for {
@@ -57,10 +68,16 @@ func main() {
 			if len(swatAlerts) == 0 {
 				return
 			}
-			index := rand.Intn(len(swatAlerts))
+			source := rand.NewSource(time.Now().UnixNano())
+			random := rand.New(source)
+
+			headerIndex := random.Intn(len(headers))
+			index := random.Intn(len(swatAlerts))
+
+			header := headers[headerIndex]
 			swatAlert := swatAlerts[index]
 			swatAlerts = remove(swatAlerts, index)
-			resp, err := http.PostForm(postMessageURL, url.Values{"token": {token}, "channel": {"fake-swat"}, "text": {swatAlert}})
+			resp, err := http.PostForm(postMessageURL, url.Values{"token": {token}, "channel": {"fake-swat"}, "text": {fmt.Sprintf("<!channel> %s - %s", header, swatAlert)}})
 			defer resp.Body.Close()
 			if err != nil {
 				fmt.Printf("An error occurred sending the message - %s", err.Error())
